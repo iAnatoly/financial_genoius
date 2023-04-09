65a59d7f9d3582e9147f01fb8cd7d342204861d5
Anatoly Ivanov <anatoly.ivanov@gmail.com>
2023-04-09 09:48:59 -0700

    emulating financial genius of Warren Buffet in python

---
+++ financial_genius.py
@@ -0,0 +1,25 @@
+import random
+
+def is_winner():
+    return random.random() < 0.5
+
+num_players = 1024
+num_rounds = 10
+
+winnings = [0] * num_players
+
+# Simulate the game for num_rounds rounds
+for round in range(num_rounds):
+    # Simulate each player's turn
+    for player in range(num_players):
+        if is_winner():
+            winnings[player] += 1
+
+highest_winners = sorted(range(num_players), key=lambda i: winnings[i], reverse=True)[:10]
+
+# Print the list of highest winners in descending order
+print("The 10 highest winners are:")
+for i, player in enumerate(highest_winners):
+    print(f"{i+1}. Player {player+1} with {winnings[player]} winnings")
+    if winnings[player]==num_rounds:
+        print('  ^^^ found Warren Buffet')
